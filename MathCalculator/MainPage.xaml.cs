5cf794a096b9c7d469a52548657186a55e94ec3f
Jack Rowe <rowejac@sheridancollege.ca>
2026-02-11 16:06:38 -0500

    Added calculator functionality

--- MathCalculator/MainPage.xaml.cs
+++ MathCalculator/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MathCalculator;
+﻿using System.Diagnostics;
+
+namespace MathCalculator;
 
 public partial class MainPage : ContentPage
 {
@@ -17,22 +19,90 @@ public partial class MainPage : ContentPage
         double rightOperand = double.Parse(_txtRightOp.Text);
 
         string opInput = (string)_pckOperand.SelectedItem;
+        char op = opInput[0];
 
         //Check the chosen operator and perform the corresponding operator.
-        double result = PerformArithmeticOperation(opInput, leftOperand, rightOperand); 
+        double result = PerformArithmeticOperation(op, leftOperand, rightOperand); 
 
 
         //Display the arithmetic expression in the output control.
+        string expression = $"{leftOperand} {opInput} {rightOperand} = {result}";
         _txtMathExp.Text = result.ToString(); 
+
 
     }
 
-    private double PerformArithmeticOperation(string opInput, double leftOperand, double rightOperand)
+    private double PerformArithmeticOperation(char op, double leftOperand, double rightOperand)
     {
+        //Check the type of operand (with op) and perform the corresponding operation
 
-        //TODO: Implement the calculation of the operands and return the result.
+        double result;
 
-        return 4.125;
-        throw new NotImplementedException();
+        switch (op)
+        {
+            case '+':
+                result = PerformAddition(leftOperand, rightOperand);
+                break;
+
+            case '-':
+                result = PerformSubtraction(leftOperand, rightOperand);
+                break;
+
+            case '*':
+                result = PerformMultiplication(leftOperand, rightOperand);
+                break;
+
+            case '/':
+                result = PerformDivision(leftOperand, rightOperand);
+                break;
+
+            case '%':
+                result = PerformDivRemainder(leftOperand, rightOperand);
+                break;
+
+            default:
+                Debug.Assert(false, "Unknown Arithmetic operation. Default result returned.");
+                result = 0;
+                break;
+
+
+        }
+
+        return result;
+    }
+
+    private double PerformDivision(double leftOperand, double rightOperand)
+    {
+        double result;
+        result = leftOperand + rightOperand;
+        return result;
+    }
+
+    private double PerformMultiplication(double leftOperand, double rightOperand)
+    {
+        double result;
+        result = leftOperand * rightOperand;
+        return result;
+    }
+
+    private double PerformDivRemainder(double leftOperand, double rightOperand)
+    {
+        double result;
+        result = leftOperand % rightOperand;
+        return result;
+    }
+
+    private double PerformSubtraction(double leftOperand, double rightOperand)
+    {
+        double result;
+        result = leftOperand - rightOperand;
+        return result;
+    }
+
+    private double PerformAddition(double leftOperand, double rightOperand)
+    {
+        double result;
+        result = leftOperand + rightOperand;
+        return result;
     }
 }
