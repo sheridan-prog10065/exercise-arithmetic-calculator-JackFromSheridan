5a2284ce3669e04e1b44973cba37d0341e5214e1
Jack Rowe <rowejac@sheridancollege.ca>
2026-02-11 15:09:21 -0500

    Added code to the OnCalculate event

--- MathCalculator/MainPage.xaml.cs
+++ MathCalculator/MainPage.xaml.cs
@@ -9,6 +9,30 @@ public partial class MainPage : ContentPage
 
     private void OnCalculate(object sender, EventArgs e)
     {
+        // Obtain user input from _txtLeftOp _txtRightOp and _pckOperand.
 
+        string leftOperandInput = _txtLeftOp.Text;
+        double leftOperand = double.Parse(leftOperandInput);
+        
+        double rightOperand = double.Parse(_txtRightOp.Text);
+
+        string opInput = (string)_pckOperand.SelectedItem;
+
+        //Check the chosen operator and perform the corresponding operator.
+        double result = PerformArithmeticOperation(opInput, leftOperand, rightOperand); 
+
+
+        //Display the arithmetic expression in the output control.
+        _txtMathExp.Text = result.ToString(); 
+
+    }
+
+    private double PerformArithmeticOperation(string opInput, double leftOperand, double rightOperand)
+    {
+
+        //TODO: Implement the calculation of the operands and return the result.
+
+        return 4.125;
+        throw new NotImplementedException();
     }
 }
